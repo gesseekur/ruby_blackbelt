818e9f65999f4190eaaf569f0d5726105940fe62
Jessica Peng <jessicaspeng@gmail.com>
2016-01-22 13:06:53 -0800

    updated css

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,20 +2,14 @@ class HistoriesController < ApplicationController
   def create
   	lender = Lender.find(params[:lender_id])
   	borrower = Borrower.find(params[:borrower_id])
-  	if lender.money > (params[:amount].to_f)
+  	if lender.money > (params[:amount].to_f) && borrower.money > (params[:amount].to_f)
 	  	history = History.create(amount: params[:amount], lender_id: params[:lender_id], borrower_id: params[:borrower_id])
 	  	borrower.update(raised:(params[:raised].to_f) + (params[:amount].to_f))
 	  	lender.update(money: lender.money - (params[:amount].to_f))
-
-	  	if borrower.money > (params[:amount].to_f)
-	  		borrower.update(money: borrower.money - (params[:amount].to_f))
-	  		redirect_to :back
-	  	else 
-	  		flash[:over] = "This will be over the amount the borrower needs"
-	  		redirect_to :back
-	  	end 	  
+		borrower.update(money: borrower.money - (params[:amount].to_f))
+	  	redirect_to :back
 	else 
-		flash[:msg] = "Insufficient funds"
+		flash[:msg] = "Insufficient funds/Over the amount the Borrower needs"
 		redirect_to :back
 	end
 	  
